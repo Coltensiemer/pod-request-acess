a7747faebf3bf1783ed893e67234fb8e9291290b
Colten Siemer <110078481+Coltensiemer@users.noreply.github.com>
2023-02-19 22:58:44 -0500

    started form validation

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,31 @@ import Logos from "./Logos";
 
 export default function App(){ 
 
+  const [formData, setData] = React.useState({
+    email:"", 
+    vaild: false,
+  })
+
+  function handleChange (event) { 
+    setData(preform => {
+      return { 
+        ...preform, 
+        [event.target.name]: event.target.value
+      }
+    }); 
+  }
+
+  function handleSubmit (event) { 
+    event.preventDefault(); 
+    console.log(formData)
+    
+  }
+  
+  console.log(formData.validity); 
+
+
   return( 
+
 <main>
     <img src={LogoImg} className="m-auto py-14"></img>
     <h1>Publish your podcasts</h1>
@@ -16,9 +40,19 @@ export default function App(){
     Apple Podcasts, Google Podcasts, Pocket Casts and more!</p>
   
   <Logos /> 
-  
-  <input type="text" placeholder="Email address"></input>
-    <button>Request access</button>
+  <form onSubmit={handleSubmit}>
+    <label htmlFor="email"></label>
+  <input id="email" 
+  name="email"
+   type="text" 
+   placeholder="Email address" 
+   onChange={handleChange}
+   value={formData.email}>
+   </input>
+   {!formData.vaild && <p className='form-error'> Ops! Please Check your email</p>}
+    <button className='form-btn' >Request access</button>
+  </form> 
+
 
  </main> 
   )
