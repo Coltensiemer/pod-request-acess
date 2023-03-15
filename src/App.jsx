cdb3b27d23d5f055a7a4ea2a781d43f9010feb35
Colten Siemer <110078481+Coltensiemer@users.noreply.github.com>
2023-03-14 23:11:36 -0400

    worked on media.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 import LogoImg from "./assets/desktop/logo.svg";
 import Logos from "./Logos";
-import imageHost from "./assets/desktop/image-host.jpg";
+import imageHostDesk from "./assets/desktop/image-host.jpg";
+import imageHostMobile from "./assets/mobile/image-host.jpg";
+import imageHostTablet from "./assets/tablet/image-host.jpg";
+
 import validator from "validator";
-import Footer from "./Footer"; 
+import Footer from "./Footer";
 
 export default function App() {
   //Sets state for email, and if it is true
@@ -14,8 +17,7 @@ export default function App() {
 
   function handleChange(event) {
     setData((preform) => {
-
-      // GOing to return the array with the new target value 
+      // GOing to return the array with the new target value
       return {
         ...preform,
         [event.target.name]: event.target.value,
@@ -35,52 +37,73 @@ export default function App() {
     }));
   }
 
+  //Handles the meida query for background images.
+  const [mediaWidth, setMediaWidth] = React.useState(window.innerWidth);
+  // UseEffect to set mediaquery state with a function. Function is put into an event listener that listens for resizes and then removes effect listener
 
+  React.useEffect(() => {
+    const handlemediaWidth = () => {
+      setMediaWidth(window.innerWidth);
+    };
+
+    window.addEventListener("resize", handlemediaWidth);
+
+    return () => {
+      window.removeEventListener("resize", handlemediaWidth);
+    };
+  }, []);
+
+  console.log(mediaWidth);
 
   return (
     <>
-    <img className='background' src={imageHost}></img>
-    <main>
-      
-      <img src={LogoImg} ></img>
-      <h1>
-        PUBLISH YOUR PODCASTS
-        <br></br>
-        <br></br>
-        <span>EVERYWHERE.</span>
-      </h1>
-      <p>
-        Upload your audio to Pod with a single click. We’ll then distribute your
-        podcast to Spotify, Apple Podcasts, Google Podcasts, Pocket Casts and
-        more!
-      </p>
+      {mediaWidth >= 800 && (
+        <img className="backgroundlg" src={imageHostDesk}></img>
+      )}
+      {mediaWidth > 699 && mediaWidth < 800 && (
+        <img className="backgroundmd" src={imageHostTablet}></img>
+      )}
+      {mediaWidth <= 699 && (
+        <img className="backgroundsm" src={imageHostMobile}></img>
+      )}
+      <main>
+        <img src={LogoImg}></img>
+        <h1>
+          PUBLISH YOUR PODCASTS
+          <br></br>
+          <br></br>
+          <span>EVERYWHERE.</span>
+        </h1>
+        <p>
+          Upload your audio to Pod with a single click. We’ll then distribute
+          your podcast to Spotify, Apple Podcasts, Google Podcasts, Pocket Casts
+          and more!
+        </p>
 
-      <section className="form-section">
-        <Logos />
-        {/* This is the input/button for form  */}
-        <form onSubmit={handleSubmit}>
-          <label htmlFor="email"></label>
-          <input
-            id="email"
-            className="input-info"
-            name="email"
-            type="text"
-            placeholder="Email address"
-            onChange={handleChange}
-            value={formData.email}
-          ></input>
+        <section className="form-section">
+          <Logos />
+          {/* This is the input/button for form  */}
+          <form onSubmit={handleSubmit}>
+            <label htmlFor="email"></label>
+            <input
+              id="email"
+              className="input-info"
+              name="email"
+              type="text"
+              placeholder="Email address"
+              onChange={handleChange}
+              value={formData.email}
+            ></input>
 
-          <button className="form-btn">Request access</button>
-          {formData.valid === false && (
-            <p className="form-error">Ops! Please Check your email</p>
-          )}
-        </form>
-        {/* END FORM  */}
-      </section>
-      
-    </main>
-    <Footer />
-   
-     </>
+            <button className="form-btn">Request access</button>
+            {formData.valid === false && (
+              <p className="form-error">Ops! Please Check your email</p>
+            )}
+          </form>
+          {/* END FORM  */}
+        </section>
+      </main>
+      <Footer />
+    </>
   );
 }
